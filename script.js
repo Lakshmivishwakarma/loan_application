c8892b6b655284800154e9a8946482b0c9a5741d
Lakshmivishwakarma <lvlaxmi.1995@gmail.com>
2022-07-08 22:39:23 +0530

    captcha validation added

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+let captchaFirst=  Math.floor(1+Math.random()*100);
+console.log(captchaFirst);
+
+let captchaSecond=  Math.floor(1+Math.random()*100);
+console.log(captchaSecond);
+// displaying captcha values on UI
+document.getElementById("firstNumber").innerHTML=captchaFirst;
+document.getElementById("secondNumber").innerHTML=captchaSecond;
 function validateForm() {
-    //get data from form
+
+//validating captcha first
+
+const userCaptcha = document.getElementById("captchaAnswer").value;
+if (parseInt(userCaptcha) !== captchaFirst+captchaSecond) {
+    alert("Invalid captcha");
+    return false;
+}
+  //get data from form
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let pan = document.getElementById("pan").value;
   let loanAmount = document.getElementById("email").value;
-// validate data
+  // validate data
 
+  // if success then return true else return false
 
-// if success then return true else return false
-
-
-  let person= {
+  let person = {
     name,
     email,
     pan,
@@ -19,5 +33,3 @@ function validateForm() {
   console.log(person);
   return true;
 }
-
-
