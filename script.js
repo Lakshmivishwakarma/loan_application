34bbf7f00c65e33fe23a6bbd0c7417f543a57223
Lakshmivishwakarma <lvlaxmi.1995@gmail.com>
2022-07-10 13:55:38 +0530

    validation updated

--- script.js
+++ script.js
@@ -6,6 +6,62 @@ console.log(captchaSecond);
 // displaying captcha values on UI
 document.getElementById("firstNumber").innerHTML = captchaFirst;
 document.getElementById("secondNumber").innerHTML = captchaSecond;
+
+function validateEmail() {
+  let email = document.getElementById("email").value;
+  if (email.length === 0) {
+    alert("Please Enter Valid Email ID");
+    return false;
+  } else {
+    // validate email pattern
+  }
+}
+
+function validateName() {
+  let name = document.getElementById("name").value;
+  if (name.length === 0) {
+    alert("Please Enter Valid name");
+    return false;
+  } else {
+    const names = name.split(" ");
+    if (names.length < 2) {
+      alert("minimum 2 words required in full name");
+      return false;
+    } else if (names[0].length < 4 || names[1].length < 4) {
+      alert("Each words should have minimum 4 character in full name ");
+      return false;
+    }
+  }
+}
+
+function validatePan() {
+  let pan = document.getElementById("pan").value;
+  if (pan.length === 0) {
+    alert("Please Enter Valid PAN Number");
+    // document.getElementById("pan").focus();
+    return false;
+  } else {
+    // validate PAN pattern
+    const pattern = /([a-zA-Z]){5}([0-9]{4})[A-Z]/;
+    if (!pattern.test(pan)) {
+      alert("Invalid PAN Format");
+      // document.getElementById("pan").focus();
+    }
+  }
+}
+
+function validateLoanAmount() {
+
+  let loanAmount = document.getElementById("loan_amount").value;
+  if (loanAmount.length === 0) {
+    alert("Please Enter Loan Amount");
+    return false;
+  } else if (loanAmount.length > 9) {
+    alert("maximum loan amount allowed 999999999");
+    return false;
+  }
+}
+
 function validateForm() {
   //validating captcha first
 
@@ -15,41 +71,13 @@ function validateForm() {
     return false;
   }
   //get data from form
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let pan = document.getElementById("pan").value;
-  let loanAmount = document.getElementById("loan_amount").value;
+
   // validate data
-  if (name.length === 0) {
-    alert("Please Enter Valid name");
-    return false;
-  }else{
-    const names=name.split(" ");
-    if(names.length<2){
-        alert("mainimum 2 words required in full name")
-        return false;
-    }else if(names[0].length<4 || names[1].length<4) {
-alert ("Each words should have minimum 4 character in full name ")
-return false;
 
-    }
-
-  }
-  if (email.length === 0) {
-    alert("Please Enter Valid Email ID");
-    return false;
-  }
-  if (pan.length === 0) {
-    alert("Please Enter Valid PAN Number");
-    return false;
-  }
-  if (loanAmount.length === 0) {
-    alert("Please Enter Loan Amount");
-    return false;
-  } else if (loanAmount.length > 9) {
-    alert ("maximum loan amount allowed 999999999")
-    return false;
-  }
+  //   if (email.length === 0) {
+  //     alert("Please Enter Valid Email ID");
+  //     return false;
+  //   }
 
   // if success then return true else return false
 
